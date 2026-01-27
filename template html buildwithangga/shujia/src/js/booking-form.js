d32a290da6dce9d3e7205393c618a25c6c04a72b
alghozii_zyy <91893301+alghoziii@users.noreply.github.com>
2026-01-27 15:46:03 +0700

    Delete template html buildwithangga/shujia directory

--- template html buildwithangga/shujia/src/js/booking-form.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('input[type="tel"]').addEventListener("input", function (e) {
-    this.value = this.value.replace(/[^0-9+]/g, "");
-});
-document.getElementsByClassName("post-code")["0"].addEventListener("input", function (e) {
-    this.value = this.value.replace(/[^0-9+]/g, "");
-});
