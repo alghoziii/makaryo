0a368bbb529d8947d6bd5f69153794923ad78a6c
alghozii_zyy <91893301+alghoziii@users.noreply.github.com>
2026-03-01 03:24:47 +0700

    Remove unused HTML files and configuration for payment and service details pages, along with Tailwind CSS and Vercel configuration.

--- template html buildwithangga/shujia/src/js/category.js
+++
@@ -1,6 +0,0 @@
-var startYourSwiper = new Swiper("#MostOrderedSlider", {
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    slidesOffsetAfter: 20,
-    slidesOffsetBefore: 20,
-});
--- template html buildwithangga/shujia/src/js/service-details.js
+++
@@ -1,6 +0,0 @@
-var startYourSwiper = new Swiper("#GreatCustomersSlider", {
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    slidesOffsetAfter: 20,
-    slidesOffsetBefore: 20,
-});
